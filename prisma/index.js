aca403e47c9c496afddb2285aed7c69b89ba9eb1
ketan mehta <meetketanmehta@gmail.com>
2024-08-04 05:20:11 +0530

    change

--- prisma/index.js
+++ prisma/index.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.getPrismaClient = exports.prisma = void 0;
 var client_1 = require("@prisma/client");
-var db = process.env.DATABASE_URL;
+var db = process.env.POSTGRES_URL;
 if (!globalThis.prisma) {
     // console.log('db in prisma index.ts', db);
     globalThis.prisma = new client_1.PrismaClient({ datasourceUrl: db });
